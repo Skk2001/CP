60631a8a3a5fce286e2ee5f3aa9c4500a2de199c
Skk2001 <72220928+Skk2001@users.noreply.github.com>
2020-10-05 20:05:53 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{	int a,b,c,d;
+	printf("enter any number 'a'\n");
+	scanf("%d",&c);
+	printf("enter any number 'b'\n");
+	scanf("%d",&d);
+//	c=a;
+//	a=b;
+//	b=c;
+//	a=a+b;
+//	b=a-b;
+//	a=a-b;
+//	c=sqrt(a);
+//	d=sqrt(b);
+//	printf("swap of a and b is\na=%d b=%d",a,b);
+//	c=c+1;
+//	c*=2;
+//	d-=2;
+	
+	printf("%d %d",c,d);
+	return 0;
+}

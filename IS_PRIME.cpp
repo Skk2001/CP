0f04d2e5aaa8d51bfd15f0bace0739da1cfd301f
Skk2001 <72220928+Skk2001@users.noreply.github.com>
2020-10-05 20:03:13 +0530

    Add files via upload

---
+++ IS_PRIME.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000007
+int main()
+{
+	long long int i,j,k=0,a;
+	vector<bool>is_p(MAX,true);
+	is_p[0]=false;
+	is_p[1]=false;
+	for(i=2;i<MAX;i++){
+		if(is_p[i]==true){
+			for(j=i*i;j<MAX;j+=i){
+				is_p[j]=false;
+			}
+		}
+	}
+	cin>>a;
+	for(i=1;i<=a;i++){
+		if(is_p[i])
+		k++;
+	}
+	cout<<k;
+	return 0;
+}

c1020d51463e78e70cb084373d0ca7aeff2b5a37
Shivesh0333 <68682235+Shivesh0333@users.noreply.github.com>
2020-10-05 20:15:12 +0530

    Update find_prime_.cpp

--- find_prime_.cpp
+++ find_prime_.cpp
@@ -1,19 +1,26 @@
-#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 1000007
+#define lli long long int  
+
 int main()
 {
-	int n,i,j;
-	printf("Enter the number between 1 to");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		for(j=2;j<i;j++)
-		{
-			if(i%j==0)
-			break;
-//			printf("%d",i);
+	long long int i,j,k=0,a;
+	vector<bool>is_p(MAX,true);
+	is_p[0]=false;
+	is_p[1]=false;
+	for(i=2;i<MAX;i++){
+		if(is_p[i]==true){
+			for(j=i*i;j<MAX;j+=i){
+				is_p[j]=false;
+			}
 		}
-		printf("%d",i);
 	}
+	cin>>a;
+	for(i=1;i<=a;i++){
+		if(is_p[i])
+		k++;
+	}
+	cout<<"The prime No. Betweeen 1 to"<<a<<" "<<k;
 	return 0;
 }
-
